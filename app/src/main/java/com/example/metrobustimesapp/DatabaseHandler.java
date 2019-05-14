cfa068d44d4b52541e3b24983997e21efb20b973
themonkeytoucher <mysterysteel@gmail.com>
2019-05-13 19:46:02 -0700

    Finished DatabaseHandler

--- app/src/main/java/com/example/metrobustimesapp/DatabaseHandler.java
+++ app/src/main/java/com/example/metrobustimesapp/DatabaseHandler.java
@@ -12,16 +12,17 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     static int  VERSION;
     String DB_NAME;
 
-
+    //Database stores BUSID (INT), HASHTABLE(TEXT)
     public DatabaseHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
         ctx = context;
         VERSION = version;
         DB_NAME = name;
     }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE ImageDB(Id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, image BLOG);");
+        db.execSQL("CREATE TABLE " + DB_NAME+ "(BUSID INTEGER, HASHTABLE TEXT);");
         Toast.makeText(ctx, "TABLE IS CREATED", Toast.LENGTH_LONG).show();
     }
 
@@ -31,15 +32,13 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         database.execSQL(sql);
     }
 
-    public void insertData(String title, byte[] image){
+    public void insertData(int busID, String hashtable){
         SQLiteDatabase database = getWritableDatabase();
-        String sql = "INSERT INTO ImageDB VALUES (NULL, ?, ?)";
+        String sql = "INSERT INTO " + DB_NAME + " VALUES (" + busID + ", ?)";
 
         SQLiteStatement statement = database.compileStatement(sql);
         statement.clearBindings();
-
-        statement.bindString(1, title);
-        statement.bindBlob(2, image);
+        statement.bindString(1, hashtable);
 
         statement.executeInsert();
     }
@@ -51,10 +50,10 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
     }
 
-    public int delete(String ID, String name) {
+    public int delete(String ID) {
         try {
             SQLiteDatabase db = getReadableDatabase();
-            db.execSQL("DELETE FROM ImageDB WHERE Id = \"" + ID + "\" OR name = \"" + name + "\";");
+            db.execSQL("DELETE FROM " + DB_NAME + " WHERE ID = \"" + ID + "\";");
             return 1;
         } catch (Exception e){
             e.printStackTrace();
@@ -64,8 +63,8 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
     public void clean(){
         SQLiteDatabase db = getWritableDatabase();
-        db.execSQL("delete from ImageDB");
-        db.delete("SQLITE_SEQUENCE", "NAME = ?", new String[]{"ImageDB"});
+        db.execSQL("delete from " + DB_NAME);
+        db.delete("SQLITE_SEQUENCE", "NAME = ?", new String[]{DB_NAME});
     }
 
     //Got from class
@@ -73,7 +72,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         //DROP TABLE IF EXISTS NAME_TABLE;
         if (VERSION == oldVersion) {
-            db.execSQL("DROP TABLE IF EXISTS ImageDB;");
+            db.execSQL("DROP TABLE IF EXISTS " + DB_NAME + ";");
             VERSION = newVersion;
             onCreate(db);
             Toast.makeText(ctx, "TABLE IS UPGRADED", Toast.LENGTH_LONG).show();
