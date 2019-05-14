4feacfc0514d44e123952f92b0df9dfa71cabdcb
themonkeytoucher <mysterysteel@gmail.com>
2019-05-14 07:15:33 -0700

    changed

--- app/src/main/java/com/example/metrobustimesapp/ViewAllStopsActivity.java
+++ app/src/main/java/com/example/metrobustimesapp/ViewAllStopsActivity.java
@@ -7,9 +7,13 @@ import android.view.View;
 import android.widget.GridView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
-import java.util.Hashtable;
 import java.util.List;
+
+import static java.util.Calendar.HOUR_OF_DAY;
 
 public class ViewAllStopsActivity extends AppCompatActivity {
 
@@ -36,9 +40,24 @@ public class ViewAllStopsActivity extends AppCompatActivity {
         while(cursor.moveToNext()){
             int id = cursor.getInt(0);
             HashMap<String, List<String>> hash = MainActivity.stringToHash(cursor.getString(1));
-            
-            list.add(new BusTimeGUI(id, hash, Integer.toString(id)));
+            for(String key: hash.keySet()){
+                String busTimes = formBusTimeString(hash, key);
+                list.add(new BusTimeGUI(id, busTimes, Integer.toString(id)));
+            }
         }
+    }
+
+    //Input: HashMap, key
+    //Output: String in the form of "time, next_time minutes" where time and next time are the next bus times
+    //Called from ViewAllStopActivity.onCreate
+    //Takes in the hash map and grabs the next two closest time.
+    protected static String formBusTimeString(HashMap<String, List<String>> hash, String key){
+
+        GregorianCalendar gcal = new GregorianCalendar();
+        Calendar cal = GregorianCalendar.getInstance();
+        int currHour = gcal.get(cal.HOUR_OF_DAY);
+        int currMin = gcal.get(cal.MINUTE);
+        
     }
 
 
